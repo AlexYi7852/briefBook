f6f4e809e95c6a4919ce088188b709e1089071a9
易烨 <yiye@yiyedeMacBook-Pro.local>
2018-12-16 23:00:13 +0800

    input 实现了数据双向绑定

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,12 +1,39 @@
 import React, { Component, Fragment } from 'react';
+import { Button, InputItem } from 'antd-mobile'
 
 class ToDoList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      list: [],
+      inputValue: '请输入'
+    }
+  }
+
   render() {
     return (
       <Fragment>
         <h1>hello yiye</h1>
+        <InputItem
+          placeholder={ this.state.inputValue }
+          onChange={ this.handlerInputChange }
+        >
+          添加
+        </InputItem>
+        <Button>提交</Button>
+        <ul>
+          <li>学习英语</li>
+          <li>learning React</li>
+        </ul>
       </Fragment>
     );
+  }
+
+  handlerInputChange = e => {
+    console.log(e, 'handlerInputChange')
+    this.setState({
+      inputValue: e
+    })
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ToDoList from './App';
+import ToDoList from './ToDoList';
 
 ReactDOM.render(<ToDoList />, document.getElementById('root'));
