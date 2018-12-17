ee8a188ddd69e07dce70326ea50daec9d781e749
易烨 <yiye@yiyedeMacBook-Pro.local>
2018-12-17 12:27:54 +0800

    list添加删除功能

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -6,33 +6,54 @@ class ToDoList extends Component {
     super(props)
     this.state = {
       list: [],
-      inputValue: '请输入'
+      inputValue: ''
     }
   }
 
   render() {
     return (
       <Fragment>
-        <h1>hello yiye</h1>
+        <h1>Learning React</h1>
         <InputItem
-          placeholder={ this.state.inputValue }
-          onChange={ this.handlerInputChange }
-        >
-          添加
-        </InputItem>
-        <Button>提交</Button>
+          value={ this.state.inputValue }
+          onChange={ v => this.handlerInputChange(v) }
+        />
+        <Button onClick={ this.handleButtonClick }>提交</Button>
         <ul>
-          <li>学习英语</li>
-          <li>learning React</li>
+          {
+            this.state.list.map((item, index) => {
+              return <li 
+                      key={ index }
+                      onClick={ () => this.handleItemDelete(index) }
+                    >
+                      { item }
+                    </li>
+            })
+          }
         </ul>
       </Fragment>
     );
   }
 
-  handlerInputChange = e => {
-    console.log(e, 'handlerInputChange')
+  handlerInputChange (e) {
     this.setState({
       inputValue: e
+    })
+  }
+
+  handleButtonClick = () => {
+    this.setState({
+      inputValue: '',
+      list: [...this.state.list, this.state.inputValue]
+    })
+  }
+
+  handleItemDelete (index) {
+    console.log('del>>>>')
+    const list = [...this.state.list]
+    list.splice(index, 1)
+    this.setState({
+      list: list
     })
   }
 }
