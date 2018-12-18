c4507d1e4e19ed6cf011b9d488dccabfff3eacb9
易烨 <yiye@yiyedeMacBook-Pro.local>
2018-12-19 00:54:04 +0800

    通过redux新增列表添加功能

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -7,23 +7,24 @@ class ToDoList extends Component {
     constructor (props) {
         super(props)
         this.state = store.getState()
+        store.subscribe(this.handleStoreChange)
     }
 
     render () {
         return (
             <Fragment>
                 <Input 
-                    value={this.state.inputValue}
-                    style={{width: 300, margin: 10}}
+                    value={ this.state.inputValue }
+                    style={{ width: 300, margin: 10 }}
                     placeholder='todo info'
                     onChange={ this.handleInputChange }
                 />
-                <Button type='primary'>提交</Button>
+                <Button type='primary' onClick={ this.handleAddItem }>提交</Button>
                 <List
                     bordered
-                    dataSource={this.state.list}
-                    style={{width: 300, margin: 10}}
-                    renderItem={item => (<List.Item>{item}</List.Item>)}
+                    dataSource={ this.state.list }
+                    style={{ width: 300, margin: 10 }}
+                    renderItem={ item => (<List.Item>{ item }</List.Item>)}
                 >
 
                 </List>
@@ -31,8 +32,23 @@ class ToDoList extends Component {
         )
     }
 
-    handleInputChange = () => {
-        console.log('hello helo')
+    handleInputChange = (e) => {
+        let action = {
+            type: 'input_change',
+            inputValue: e.target.value
+        }
+        store.dispatch(action)
+    }
+
+    handleAddItem = () => {
+        this.setState((prevState) => ({
+            inputValue: '',
+            list: [ ...prevState.list, prevState.inputValue]
+        }))
+    }
+
+    handleStoreChange = () => {
+        this.setState(store.getState())
     }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,5 +10,11 @@ const defaultState = {
 }
     
 export default (state = defaultState, action) => {
+    const newState = state
+    console.log(newState, 'newState')
+    if (action.type === 'input_change') {
+        newState.inputValue = action.inputValue
+        return newState
+    }
     return state
 }
